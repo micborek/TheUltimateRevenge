21d92fc45bdb17d4f81f526ac8d97a5f63b49f92
Michal Borek <63055456+michalaborek@users.noreply.github.com>
2021-02-16 11:42:39 +0100

    main wip

--- TheUltimateRevenge.py
+++ TheUltimateRevenge.py
@@ -1,22 +1,35 @@
 import pygame as pg
-
-# game window resolution
-WIN_WIDTH = 1366
-WIN_HEIGHT = 768
+import settings as st
 
 # start the pygame
 pg.init()
-pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+WIN = pg.display.set_mode((st.WIN_WIDTH, st.WIN_HEIGHT))
+pg.display.set_caption('The Ultimate Revenge!')
+
+HERO_IMG = pg.image.load(st.HERO_IMG_PATH)
+HERO = pg.transform.scale(HERO_IMG, (55, 40))
+
+
+def draw_window():
+    """This one is for drawing the gameplay window"""
+
+    WIN.fill((255, 255, 255))
+    WIN.blit(HERO, (400, 400))
+    pg.display.update()
 
 
 def main():
     """Main function for running the game"""
 
+    clock = pg.time.Clock()
     run = True
     while run:
+        clock.tick(st.FPS)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
+
+        draw_window()
 
     pg.quit()
 
---
+++ settings.py
@@ -0,0 +1,14 @@
+"""This one is holding the values for the main game variables"""
+
+import os
+
+
+# game window resolution
+WIN_WIDTH = 1366
+WIN_HEIGHT = 768
+
+# main() loop refresh value
+FPS = 60
+
+# characters img paths
+HERO_IMG_PATH = os.path.join('source', 'img', 'hero.png')
